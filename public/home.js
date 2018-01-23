7efa701e36888a25897b0a02985362ad52906434
shashvat <lanetteam.shashvat@gmail.com>
2018-01-23 14:50:00 +0530

    script in diff js file

---
+++ public/home.js
@@ -0,0 +1,8 @@
+let socket = io();
+socket.on(`connect`,()=>{
+    console.log(`connected`);
+    socket.on;
+});
+socket.on(`disconnect`,()=>{
+    console.log(`disconnected`);
+});
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require(`path`);
 const express = require(`express`);
+const http = require(`http`);
+const socket = require(`socket.io`)
 
 const {router} = require(`../routes/routes`);
 const PublicPath = path.join(__dirname,`../public`);
@@ -10,7 +12,16 @@ let app = express();
 app.use(`/`,router);
 app.use(express.static(PublicPath));
 
+let server = http.createServer(app);
+let io = socket(server);
+io.on(`connection`,(socket)=>{
+    console.log(`new user connected!`);
+    socket.on(`disconnect`,()=>{
+        console.log(`user disconnected!`);
+    });
+});
+
 const publicPath = path.join(__dirname,'../public');
 console.log(publicPath);
 
-app.listen(Port,()=>{console.log(`app is listening on port ${Port}`)})
+server.listen(Port,()=>{console.log(`app is listening on port ${Port}`)})
